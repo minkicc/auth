54a0a0113b64abdfe44bbc64fd961b27d32a6bab
Open Source Contributors <opensource@example.com>
2025-05-07 11:05:29 +0800

    使用绝对路径

--- server/main.go
+++ server/main.go
@@ -137,9 +137,9 @@ func main() {
 
 	// 添加静态文件服务
 	// 前端静态文件
-	r.Static("/assets", "./web/dist/assets")
+	r.Static("/assets", "/app/web/assets")
 	// 添加 favicon.ico 路由
-	r.StaticFile("/favicon.ico", "./web/dist/favicon.ico")
+	r.StaticFile("/favicon.ico", "/app/web/favicon.ico")
 	// 将前端其他请求重定向到index.html以支持单页应用
 	r.NoRoute(func(c *gin.Context) {
 		// 如果是API请求，返回404
@@ -155,7 +155,7 @@ func main() {
 		}
 		log.Println("Redirecting to index.html")
 		// 其他所有请求返回前端index.html
-		c.File("./web/dist/index.html")
+		c.File("/app/web/dist/index.html")
 	})
 
 	// Create main HTTP server
