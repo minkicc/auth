40cbbea83d65b2fb8371cc58535d946fe5a0f36b
Open Source Contributors <opensource@example.com>
2025-07-23 13:09:36 +0800

    login

--- server/common/consts.go
+++ server/common/consts.go
@@ -15,4 +15,5 @@ const (
 	RedisKeyVerificationToken = "auth_verification_token:"
 	RedisKeyEmailPreregister  = "auth_email_prereg:"
 	RedisKeyPhonePreregister  = "auth_phone_prereg:"
+	RedisKeyOauthCode         = "auth_oauth_code:"
 )
