33787c186bc118ccc8bb9b00d32835b080425e86
Open Source Contributors <opensource@example.com>
2025-08-05 21:34:56 +0800

    add header

--- server/config/buildconfig.go
+++ server/config/buildconfig.go
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025 Auth Contributors (https://example.com)
+ * Licensed under the MIT License.
+ */
+
 package config
 
 // 使用 -ldflags "-X 'example.com/auth/server/config.API_ROUTER_PATH=/api'" 来设置
